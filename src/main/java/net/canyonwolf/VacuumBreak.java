af6dc47f68b299a5a37de994a2d263e4b2c05add
Cody West <cww2697@gmail.com>
2025-01-27 16:48:30 -0600

    Update config.yml and main class

--- src/main/java/net/canyonwolf/VacuumBreak.java
+++ src/main/java/net/canyonwolf/VacuumBreak.java
@@ -1,19 +1,19 @@
 package net.canyonwolf;
 
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class VacuumBreak extends JavaPlugin {
-    
 
-        @Override
-        public void onEnable(){
-            saveDefaultConfig();
-            getLogger().info("Vacuum Break Enabled");
-        }
+    @Override
+    public void onEnable(){
+        saveDefaultConfig();
+        getLogger().info("Vacuum Break Enabled");
+    }
 
-        @Override
-        public void onDisable(){
-            getLogger().info("Vacuum Break Disabled");
-        }
+    @Override
+    public void onDisable(){
+        getLogger().info("Vacuum Break Disabled");
+    }
 
 }
