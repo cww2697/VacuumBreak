5b42b5929c84eaf69b4289e516e19fbb4295e577
Cody West <cww2697@gmail.com>
2025-01-27 21:04:48 -0600

    Add changes for onload

--- src/main/java/net/canyonwolf/VacuumBreak.java
+++ src/main/java/net/canyonwolf/VacuumBreak.java
@@ -1,9 +1,13 @@
 package net.canyonwolf;
 
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class VacuumBreak extends JavaPlugin {
+
+    private boolean silent;
 
     @Override
     public void onEnable(){
@@ -12,8 +16,22 @@ public class VacuumBreak extends JavaPlugin {
     }
 
     @Override
+    public void onLoad() {
+        this.loadConfig();
+        if(!getConfig().getBoolean("silent")){
+            getLogger().info("Vacuum Break Loaded");
+        }
+    }
+
+    @Override
     public void onDisable(){
         getLogger().info("Vacuum Break Disabled");
+    }
+
+    private void loadConfig(){
+        FileConfiguration config = this.getConfig();
+        this.silent = config.getBoolean("silent");
+
     }
 
 }
